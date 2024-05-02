3e2156cab873fa158108d1633100e5c0212e7f85
Alex Coronell <alexander@catapultsteel.com>
2024-05-02 17:01:00 -0500

    Cache validate added

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,15 +1,15 @@
-import { getProducts } from 'app/services/shopify/products'
+import { getMainProducts } from 'app/services/shopify/products'
 import Image from 'next/image'
 import styles from './MainProducts.module.sass'
 
 export const MainProducts = async () => {
-  const products = await getProducts()
+  const products = await getMainProducts()
 
   return (
     <section className={styles.MainProducts}>
       <h3>✨ New products released!</h3>
       <div className={styles.MainProducts__grid}>
-        {products?.map((product) => {
+        {products?.map((product: any) => {
           const imageSrc = product.images[0].src;
           return (
             <article key={product.id}>
--- src/services/shopify/products.ts
+++ src/services/shopify/products.ts
@@ -1,15 +1,17 @@
-import { env } from "app/config/env"
-import { shopifyUrls } from "./urls"
+import { env } from "app/config/env";
+import { shopifyUrls } from "./urls";
 
 export const getProducts = async (id?: string): Promise<ProductType[]> => {
   try {
-    const apiUrl = id ? `${shopifyUrls.products.all}?ids=${id}` : shopifyUrls.products.all
+    const apiUrl = id
+      ? `${shopifyUrls.products.all}?ids=${id}`
+      : shopifyUrls.products.all;
     const response = await fetch(apiUrl, {
       headers: new Headers({
-        'X-Shopify-Access-Token': env.SHOPIFY_TOKEN
-      })
-    })
-    const { products } = await response.json()
+        "X-Shopify-Access-Token": env.SHOPIFY_TOKEN,
+      }),
+    });
+    const { products } = await response.json();
 
     const transformedProducts = products.map((product: any) => {
       return {
@@ -22,10 +24,32 @@ export const getProducts = async (id?: string): Promise<ProductType[]> => {
         quantity: product.variants[0].inventory_quantity,
         handle: product.handle,
         tags: product.tags,
-      }
-    })
-    return transformedProducts
+      };
+    });
+    return transformedProducts;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
+
+export const getMainProducts = async () => {
+  const response = await fetch(shopifyUrls.products.mainProducts, {
+    headers: new Headers({
+      "X-Shopify-Access-Token": env.SHOPIFY_TOKEN,
+    }),
+
+    /* Cache Options */
+    // next: {
+    // revalidate: 60
+    // }
+    // cache: 'no-cache'
+    cache: "force-cache",
+    next: {
+      tags: ['main-products']
+    }
+  });
+
+  const { products } = await response.json();
+
+  return products;
+};
